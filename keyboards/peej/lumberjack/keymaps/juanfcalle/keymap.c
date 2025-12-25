2a3dd67c36dd7c98382e7b4dd66362ae1ec6199c
Juan Calle <juan.calle@lufthansa-group.com>
2025-12-25 20:45:22 +0100

    Update lumberjack keymaps

--- keyboards/peej/lumberjack/keymaps/juanfcalle/keymap.c
+++ keyboards/peej/lumberjack/keymaps/juanfcalle/keymap.c
@@ -31,23 +31,45 @@ enum custom_keycodes {
 #define LOWER MO(_LOWER)
 #define HIGHER MO(_HIGHER)
 
-#define CTL_R MT(MOD_LCTL, KC_R)
-#define ALT_S MT(MOD_LALT, KC_S)
-#define CMD_TE MT(MOD_LGUI, KC_T)
+// Home Row Mod layer Base
+#define HRM_MEH_BSPC MT(MOD_MEH, KC_BSPC)
+#define HRM_HYPR_A MT(MOD_HYPR, KC_A)
+#define HRM_CTL_R MT(MOD_LCTL, KC_R)
+#define HRM_ALT_S MT(MOD_LALT, KC_S)
+#define HRM_CMD_T MT(MOD_LGUI, KC_T)
 
-#define ALT_EQL MT(MOD_LALT, KC_EQL)
-#define CMD_0   MT(MOD_LGUI, KC_0)
+#define HRM_CMD_N MT(MOD_RGUI, KC_N)
+#define HRM_ALT_E MT(MOD_RALT, KC_E)
+#define HRM_CTL_I MT(MOD_RCTL, KC_I)
+#define HRM_HYPR_O MT(MOD_HYPR, KC_O)
+#define HRM_MEH_ESC MT(MOD_MEH, KC_ESC)
 
-#define CMD_F10 MT(MOD_LGUI, KC_F10)
 
-#define CTL_I MT(MOD_RCTL, KC_I)
-#define ALT_E MT(MOD_RALT, KC_E)
-#define CMD_N MT(MOD_RGUI, KC_N)
+// Home Row Mod layer Lower
+#define HRM_MEH_TILD MT(MOD_MEH, KC_TILD)
+#define HRM_HYPR_QUOT MT(MOD_HYPR, KC_QUOT)
+#define HRM_CTL_LPRN MT(MOD_LCTL, KC_LPRN)
+#define HRM_ALT_EQL MT(MOD_LALT, KC_EQL)
+#define HRM_CMD_0 MT(MOD_LGUI, KC_0)
 
-#define ALT_ASTR MT(MOD_RALT, KC_PAST)
-#define CMD_1   MT(MOD_RGUI, KC_1)
+#define HRM_CMD_1 MT(MOD_RGUI, KC_1)
+#define HRM_ALT_ASTR MT(MOD_RALT, KC_PAST)
+#define HRM_CTL_RPRN MT(MOD_RCTL, KC_RPRN)
+#define HRM_HYPR_MINS MT(MOD_HYPR, KC_MINS)
+#define HRM_MEH_PIPE MT(MOD_MEH, KC_PIPE)
 
-#define CMD_F1 MT(MOD_RGUI, KC_F1)
+
+// Home Row Mod layer higher
+#define HRM_HYPR_F12 MT(MOD_HYPR, KC_F12)
+#define HRM_CTL_LFT MT(MOD_LCTL, KC_LEFT)
+#define HRM_ALT_DWN MT(MOD_LALT, KC_DOWN)
+#define HRM_CMD_UP MT(MOD_LGUI, KC_UP)
+
+#define HRM_CMD_DWN MT(MOD_RGUI, KC_DOWN)
+#define HRM_ALT_UP MT(MOD_RALT, KC_UP)
+#define HRM_CTL_RGHT MT(MOD_RCTL, KC_RGHT)
+#define HRM_HYPR_F11 MT(MOD_HYPR, KC_F11)
+
 
 bool is_alt_tab_active = false;
 bool is_cmd_tab_active = false;
@@ -60,33 +82,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer
  */
 [_BASE] = LAYOUT_ortho_5x12(
-     KC_LPAD, KC_LEFT, KC_DOWN,               KC_UP,            KC_RGHT,        KC_VOLD,          KC_VOLU,          KC_LEFT,             KC_DOWN,   KC_UP, KC_RGHT,     KC_CAPS,
-     CMD_TAB,    KC_Q,    KC_W,                KC_F,               KC_P,           KC_G,             KC_J,             KC_L,                KC_U,    KC_Y, KC_SCLN,     ALT_TAB,
-     KC_BSPC,    KC_A,   CTL_R,               ALT_S,             CMD_TE,           KC_D,             KC_H,            CMD_N,               ALT_E,   CTL_I,    KC_O,      KC_ESC,
-      KC_TAB,    KC_Z,    KC_X,                KC_C,               KC_V,           KC_B,             KC_K,             KC_M,             KC_COMM,  KC_DOT, KC_SLSH,      KC_ENT,
-LSFT(KC_TAB), XXXXXXX, XXXXXXX, LT(HIGHER, KC_LEFT), LT(LOWER, KC_DOWN), LSFT_T(KC_SPC),   RSFT_T(KC_SPC), LT(LOWER, KC_UP), LT(HIGHER, KC_RGHT), XXXXXXX, XXXXXXX, RSFT(KC_ENT)
+    XXXXXXX,        XXXXXXX,       XXXXXXX,           XXXXXXX,                 XXXXXXX,                XXXXXXX,                XXXXXXX,                XXXXXXX,             XXXXXXX,               XXXXXXX,    XXXXXXX,     XXXXXXX,
+    CMD_TAB,        KC_Q,          KC_W,              KC_F,                    KC_P,                   KC_G,                   KC_J,                   KC_L,                KC_U,                  KC_Y,       KC_SCLN,     ALT_TAB,
+    HRM_MEH_BSPC,   HRM_HYPR_A,    HRM_CTL_R,         HRM_ALT_S,               HRM_CMD_T,              KC_D,                   KC_H,                   HRM_CMD_N,           HRM_ALT_E,             HRM_CTL_I,  HRM_HYPR_O,  HRM_MEH_ESC,
+    KC_TAB,         KC_Z,          KC_X,              KC_C,                    KC_V,                   KC_B,                   KC_K,                   KC_M,                KC_COMM,               KC_DOT,     KC_SLSH,     KC_ENT,
+    XXXXXXX,        XXXXXXX,       XXXXXXX,           LT(HIGHER, KC_LEFT),     LT(LOWER, KC_DOWN),     MT(MOD_LSFT, KC_SPC),   MT(MOD_RSFT, KC_SPC),   LT(LOWER, KC_UP),    LT(HIGHER, KC_RGHT),   XXXXXXX,    XXXXXXX,     XXXXXXX
 ),
 
 /*
  * Lower Layer
  */
 [_LOWER] = LAYOUT_ortho_5x12(
-    XXXXXXX, _______, _______, _______, _______, XXXXXXX,      XXXXXXX, _______,  _______, _______, _______, XXXXXXX,
-     KC_GRV, KC_PERC, KC_AMPR, KC_DQUO, KC_PLUS,   KC_AT,       KC_DLR, KC_UNDS,  KC_LBRC, KC_RBRC, KC_EXLM, KC_BSLS,
-    KC_TILD, KC_QUOT, SC_LCPO, ALT_EQL,   CMD_0, KC_LCBR,      KC_RCBR,   CMD_1, ALT_ASTR, SC_RCPC, KC_MINS, KC_PIPE,
-    _______,    KC_6,    KC_7,    KC_8,    KC_9, KC_CIRC,      KC_HASH,    KC_2,     KC_3,    KC_4,    KC_5, _______,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,      _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX, _______
+    XXXXXXX,         XXXXXXX,           XXXXXXX,               XXXXXXX,         XXXXXXX,                XXXXXXX,          XXXXXXX,          XXXXXXX,             XXXXXXX,               XXXXXXX,        XXXXXXX,        XXXXXXX,
+    KC_GRV,          KC_PERC,           KC_AMPR,               KC_DQUO,         KC_PLUS,                KC_AT,            KC_DLR,           KC_UNDS,             KC_LBRC,               KC_RBRC,        KC_EXLM,        KC_BSLS,
+    HRM_MEH_TILD,    HRM_HYPR_QUOT,     HRM_CTL_LPRN,          HRM_ALT_EQL,     HRM_CMD_0,              KC_LCBR,          KC_RCBR,          HRM_CMD_1,           HRM_ALT_ASTR,          HRM_CTL_RPRN,   HRM_HYPR_MINS,  HRM_MEH_PIPE,
+    _______,         KC_6,              KC_7,                  KC_8,            KC_9,                   KC_CIRC,          KC_HASH,          KC_2,                KC_3,                  KC_4,           KC_5,           _______,
+    XXXXXXX,         XXXXXXX,           XXXXXXX,               _______,         _______,                _______,          _______,          _______,             _______,               XXXXXXX,        XXXXXXX,        XXXXXXX
 ),
 
 /*
  * Higher Layer
  */
 [_HIGHER] = LAYOUT_ortho_5x12(
-    XXXXXXX, _______, _______, _______, _______, XXXXXXX,      XXXXXXX, _______, _______, _______, _______, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,  KC_F12, KC_LCTL, KC_LALT, CMD_F10, XXXXXXX,      KC_MPLY,  CMD_F1, KC_RALT, KC_RCTL,  KC_F11, XXXXXXX,
-    _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9, XXXXXXX,      KC_MPRV,   KC_F2,   KC_F3,   KC_F4,   KC_F5, _______,
-    _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______,      _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______
+    XXXXXXX,         XXXXXXX,       XXXXXXX,           XXXXXXX,         XXXXXXX,                XXXXXXX,          XXXXXXX,          XXXXXXX,             XXXXXXX,               XXXXXXX,        XXXXXXX,        XXXXXXX,
+    XXXXXXX,         XXXXXXX,       XXXXXXX,           XXXXXXX,         XXXXXXX,                XXXXXXX,          KC_MPRV,          KC_MPLY,             KC_MNXT,               KC_VOLD,        KC_MUTE,        KC_VOLU,
+    HRM_MEH_BSPC,    HRM_HYPR_F12,  HRM_CTL_LFT,       HRM_ALT_DWN,     HRM_CMD_UP,             KC_RGHT,          KC_LEFT,          HRM_CMD_DWN,         HRM_ALT_UP,            HRM_CTL_RGHT,   HRM_HYPR_F11,   HRM_MEH_ESC,
+    _______,         KC_F6,         KC_F7,             KC_F8,           KC_F9,                  KC_F10,           KC_F1,            KC_F2,               KC_F3,                 KC_F4,          KC_F5,          _______,
+    XXXXXXX,        XXXXXXX,        XXXXXXX,           _______,         _______,                _______,          _______,          _______,             _______,               XXXXXXX,        XXXXXXX,        XXXXXXX
 ),
 
 };
